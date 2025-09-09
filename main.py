d753840fc3d1df12c038269f635b3c543bf3d01e
Emilio0212 <194541-4@iberoleon.edu.mx>
2025-09-09 13:52:39 -0600

    Version 2

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from clases.operaciones import Operaciones
+from clases.operaciones import NumeroMayor
 
 
 def main():
@@ -13,3 +14,11 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+# Extraer el número mayor de una lista
+def prueba_numero_mayor():
+    lista = [3, 2, 5, 1, 4]
+    numero_mayor_instancia = NumeroMayor(lista)
+    print("El número mayor es:", numero_mayor_instancia.numero_mayor())
+    prueba_numero_mayor()
+    
