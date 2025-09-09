62c067b88876c155e49e6c6318a5b8a53b545ec4
Bruno Ramos <193126-7@iberoleon.edu.mx>
2025-09-09 13:51:24 -0600

    Agregación función serieFibonacci()

--- clases/operaciones.py
+++ clases/operaciones.py
@@ -1,10 +1,13 @@
 class Operaciones:
     def __init__(self):
         self.alejandroMontes = "Alejandro Montes"
-        
-    
+
     def saludoAlejandroMontes(self):
         return "Mi nombre es " + self.alejandroMontes
 
-    def ordenarLista(self, lista):
-        return sorted(lista)
+    def serieFibonacci(self, n):
+        a, b = 0, 1
+        while a <= n:
+            print(a, end=" ")
+            a, b = b, a + b
+        print()
