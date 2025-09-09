1853a926bf172c722367a917605ac75dd67d336f
Julio Rangel <juliojeff10@hotmail.com>
2025-09-09 13:30:40 -0600

    Agregue remplazarEspacios Julio Rangel

--- clases/operaciones.py
+++ clases/operaciones.py
@@ -5,6 +5,10 @@ class Operaciones:
     
     def saludoAlejandroMontes(self):
         return "Mi nombre es " + self.alejandroMontes
+    
+    def reemplazarEspacios(self, texto: str, caracter: str) -> str:
+        
+        if not caracter or len(caracter) != 1:
+            raise ValueError("El caracter debe ser un único símbolo.")
 
-    def ordenarLista(self, lista):
-        return sorted(lista)
+        return texto.replace(" ", caracter)
