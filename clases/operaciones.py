ee71e817c5a15d8010a62b9a074bb2ed7781b297
Emilio0212 <194541-4@iberoleon.edu.mx>
2025-09-09 14:07:51 -0600

    Version corregida

--- clases/operaciones.py
+++ clases/operaciones.py
@@ -5,15 +5,6 @@ class Operaciones:
     
     def saludoAlejandroMontes(self):
         return "Mi nombre es " + self.alejandroMontes
-
-    def ordenarLista(self, lista):
-        return sorted(lista)
     
-    # Extraer el número mayor de una lista
-class NumeroMayor:
-    
-    def __init__(self, lista):
-        self.lista = lista
-
     def numero_mayor(self):
         return max(self.lista)
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from clases.operaciones import Operaciones
-from clases.operaciones import NumeroMayor
 
 
 def main():
@@ -9,16 +8,12 @@ def main():
     lista=[3,2,5,1,4]
     
     #Realiza aquí tu prueba
+    test = Operaciones()
+    print(test.numero_mayor())
+    lista=[10,23,1,54,53]
 
     print(test.ordenarLista(lista))
 
 if __name__ == '__main__':
     main()
 
-# Extraer el número mayor de una lista
-def prueba_numero_mayor():
-    lista = [3, 2, 5, 1, 4]
-    numero_mayor_instancia = NumeroMayor(lista)
-    print("El número mayor es:", numero_mayor_instancia.numero_mayor())
-    prueba_numero_mayor()
-    
